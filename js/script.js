faeb0276b2dfe307ea3f852db4802a630249afe1
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-01-14 15:54:50 +0200

    Начало 7 главы

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var access =
-document.getElementById("code9");
+var access = document.getElementById("code9");
 var code = access.innerHTML;
+//используем его свойство innerHTML для получения его содержимого
 code = code + " midnight";
 alert(code);
